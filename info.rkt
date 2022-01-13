94320214261e7a2c068091c6395931acbcbd5929
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-01-14 04:51:09 +0800

    dep(gui-easy-lib)

--- info.rkt
+++ info.rkt
@@ -1,6 +1,7 @@
 #lang info
 (define collection "re-algo")
-(define deps '("base"))
+(define deps '("base"
+               "gui-easy-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define pkg-desc "Description Here")
 (define version "0.0")
